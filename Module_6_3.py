0c9289d9c6fb372bfdfc39f10b7560236a871a4f
EvgeniyPanov <Zhenya_panov_1994@mail.ru>
2024-11-14 19:18:13 +0300

    Update Module_6_3.py

--- Module_6_3.py
+++ Module_6_3.py
@@ -1,10 +1,11 @@
 import random
 
 class Animal:
+    live = True
+    sound = None
+    _DEGREE_OF_DANGER = 0
+
     def __init__(self, speed):
-        self.live = True
-        self.sound = None
-        self._DEGREE_OF_DANGER = 0
         self._cords = [0, 0, 0]
         self.speed = speed
 
@@ -20,7 +21,7 @@ class Animal:
         print(f"X: {self._cords[0]}, Y: {self._cords[1]}, Z: {self._cords[2]}")
 
     def attack(self):
-        if self._DEGREE_OF_DANGER < 5:
+        if self._DEGREE_OF_DANGER >= 5:
             print("Be careful, i'm attacking you 0_0")
         else:
             print("Sorry, i'm peaceful :)")
@@ -32,36 +33,37 @@ class Animal:
             print("This animal doesn't make any sound")
 
 class Bird(Animal):
+    beak = True
+
     def __init__(self, speed):
         super().__init__(speed)
-        self.beak = True
 
     def lay_eggs(self):
         eggs = random.randint(1, 4)
         print(f"Here are{'is' if eggs == 1 else ''} {eggs} egg{'s' if eggs > 1 else ''} for you")
 
 class AquaticAnimal(Animal):
+    _DEGREE_OF_DANGER = 3
+
     def __init__(self, speed):
         super().__init__(speed)
-        self._DEGREE_OF_DANGER = 3
 
     def dive_in(self, dz):
-        dz_mod = abs(dz)
-        self._cords[2] -= dz_mod * 2 * self.speed
-        if self._cords[2] < 0:
-            self._cords[2] = 0
+        self._cords[2] += dz * self.speed
 
 class PoisonousAnimal(Animal):
+    _DEGREE_OF_DANGER = 8
+
     def __init__(self, speed):
         super().__init__(speed)
-        self._DEGREE_OF_DANGER = 8
 
-class Duckbill(Bird, AquaticAnimal, PoisonousAnimal):
+class Duckbill(AquaticAnimal, Bird, PoisonousAnimal):
+    sound = "Click-click-click"
+
     def __init__(self, speed):
         super().__init__(speed)
-        self.sound = "Click-click-click"
 
-# Пример работы программы:
+
 db = Duckbill(10)
 
 print(db.live)
